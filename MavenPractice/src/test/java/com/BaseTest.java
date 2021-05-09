45f3465da88b86012578d1cc23d6d9c7eafd3b2d
Saiful <saiful@gmail.com>
2021-05-09 09:51:22 -0400

    new file

---
+++ MavenPractice/src/test/java/com/BaseTest.java
@@ -0,0 +1,30 @@
+package com;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+
+	WebDriver Driver;
+	
+	@Test
+	public void openBrowser() {
+	
+	WebDriverManager.chromedriver().setup();	
+	Driver = new ChromeDriver();
+	
+    Driver.get("http://www.google.com");
+		
+	}
+	
+//	@Test
+//	public void test() {
+//		
+//	System.out.println("test1");
+//		
+//	}
+//	
+}
